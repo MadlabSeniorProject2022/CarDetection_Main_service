3736f7b43156e26dc728cc14b254a022d3d22b26
tamitamboommm!! <pun.hoo.hoo@gmail.com>
2023-02-20 23:08:13 +0700

    improve: new flow

---
+++ lean_detect.py
@@ -0,0 +1,100 @@
+import os
+
+import torch
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+random.seed(1)
+
+class UseModel:
+    def __init__(self, weights, device = "", confident = 0.25, iou = 0.45, detect_class = None, normalize_size = 640, is_trace = True) -> None:
+        set_logging()
+        self.device = select_device(device)
+        self.half = self.device.type != 'cpu'  # half precision only supported on CUDA
+        self.norm_size = normalize_size
+        self.detect_class = detect_class
+        self.confident = confident
+        self.iou = iou
+        # Load model
+        model = attempt_load(weights, map_location=self.device)  # load FP32 model
+        self.stride = int(model.stride.max())  # model stride
+        self.norm_size= check_img_size(self.norm_size, s=self.stride)  # check img_size
+
+        if is_trace:
+            model = TracedModel(model, self.device, self.norm_size)
+
+        if self.half:
+            model.half()  # to FP16
+        
+        if self.device.type != 'cpu':
+            model(torch.zeros(1, 3, self.norm_size, self.norm_size).to(self.device).type_as(
+                next(model.parameters())))  # run once
+        
+        self.model = model
+
+    def detect (self, source, do_function = None):
+        old_img_w = old_img_h = self.norm_size
+        old_img_b = 1
+        dataset = LoadImages(source, img_size=self.norm_size, stride=self.stride)
+        have: bool = False
+        for path, img, im0s, vid_cap in dataset:
+            img = torch.from_numpy(img).to(self.device)
+            img = img.half() if self.half else img.float()  # uint8 to fp16/32
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            if img.ndimension() == 3:
+                img = img.unsqueeze(0)
+
+            # Warmup
+            if self.device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+                old_img_b = img.shape[0]
+                old_img_h = img.shape[2]
+                old_img_w = img.shape[3]
+                for i in range(3):
+                    self.model(img, augment=False)[0]
+
+            pred = self.model(img, augment=False)[0]    
+            pred = non_max_suppression(pred, self.confident, self.iou, classes=self.detect_class, agnostic=False)
+
+            # Do function that include
+            #if do_function != None:
+            #    return do_function(pred, im0s, dataset)
+            # If function not include, return as predicted value
+            #return pred
+            
+            for i, det in enumerate(pred):
+                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+                # normalization gain whwh
+                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(
+                        img.shape[2:], det[:, :4], im0.shape).round()
+                    # find biggest one
+                    sizes = [((xyxy[3] - xyxy[1])*(xyxy[2] - xyxy[0])) for *xyxy, conf, cls in reversed(det)]
+                    index = sizes.index(max(sizes))
+                    #print(sizes[index])
+                    # Write results
+                    selected = reversed(det)[index]
+                    *xyxy, conf, cls = selected
+                    #for *xyxy, conf, cls in reversed(det):
+                    #    if do_function != None:
+                    #        do_function(xyxy, im0, conf, cls)
+                    if have:
+                        #print("to f")
+                        #have = False
+                        pass
+                    else:
+                        if ((int((xyxy[3] - xyxy[1])) >= 300 and int((xyxy[2] - xyxy[0])) >= 300)) and (int(xyxy[3]) >= 600 and int(xyxy[3]) <= 900):
+                            do_function(xyxy, im0, conf, cls)
+                            have = True
+                    
+                else: 
+                    have = False
+        
--- utils/plots.py
+++ utils/plots.py
@@ -25,7 +25,6 @@ from utils.metrics import fitness
 matplotlib.rc('font', **{'size': 11})
 matplotlib.use('Agg')  # for writing to files only
 
-import utils.color_recognition as cr
 
 def color_list():
     # Return first 10 plt colors as (r,g,b) https://stackoverflow.com/questions/51350872/python-from-color-name-to-rgb
@@ -56,22 +55,17 @@ def butter_lowpass_filtfilt(data, cutoff=1500, fs=50000, order=5):
 
 
 def plot_one_box(x, img, color=None, label=None, line_thickness=3):
-    crop_img = img[ int(x[1]) : int(x[3]), int(x[0]): int(x[2])]
-    if (crop_img.shape[0] >= 230) and (crop_img.shape[1] >= 230):
-        # Plots one bounding box on image img
-        tl = line_thickness or round(0.05 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
-        color = color or [random.randint(0, 255) for _ in range(3)]
-        c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-        
-        body_color = cr.predict(crop_img)
-        print(body_color)
-        cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
-        if label:
-            tf = max(tl - 1, 1)  # font thickness
-            t_size = cv2.getTextSize((body_color), 0, fontScale=tl, thickness=tf)[0]
-            c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-            cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
-            cv2.putText(img, (body_color), (c1[0], c1[1] - 2), 0, tl, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
 def crop_plot(x, img):
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
