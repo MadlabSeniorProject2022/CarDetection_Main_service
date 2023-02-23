c13f2495ae4f4f0e32c22d897124863b74833cd6
tamitamboommm!! <pun.hoo.hoo@gmail.com>
2023-02-24 01:37:40 +0700

    feat: service update

--- app.py
+++ app.py
@@ -15,21 +15,19 @@ def process():
     if request.method == 'POST':  
         f = request.files['file']
         f.save(f"static/files/og/{f.filename}")
-        detect_flow(f"static/files/og/{f.filename}")
+        detect_flow(f"./static/files/og/{f.filename}")
         return redirect("/")
 
 
 @app.route('/show/<id>')
 def show(id):
-    print(db.get_byid(id))
     return render_template("home.html", table = list(db.get_list_data()), show = db.get_byid(id))
 
 
 @app.route('/')
 def home():
-    print(db.get_lasted())
     return render_template("home.html", table = list(db.get_list_data()), show = db.get_lasted())
 def table_event(id):
     print(id)
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True, port=8080)
